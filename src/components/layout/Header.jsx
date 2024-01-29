6e7e865ef13a132faf622882db26624e034269b8
hyeeons <qouop0101@gmail.com>
2024-01-29 15:38:56 +0900

    라우터오류 해결

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,28 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>JUST 쇼핑몰</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">상의</a>
+          </li>
+          <li>
+            <a href="/">바지</a>
+          </li>
+          <li>
+            <a href="/">아우터</a>
+          </li>
+          <li>
+            <a href="/">악세사리</a>
+          </li>
+          <li>
+            <a href="/">신발</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
