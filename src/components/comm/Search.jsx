25a0a498aa98cf8ca6c7b463ce3ae50684e2dfce
hyeeons <qouop0101@gmail.com>
2024-03-08 13:11:03 +0900

    메인 - search box 수정

--- src/components/comm/Search.jsx
+++ src/components/comm/Search.jsx
@@ -16,58 +16,60 @@ import { FiSearch } from "react-icons/fi";
 
 const Search = () => {
   return (
-    <Flex
-      bg={"white"}
-      display={"flex"}
-      h={61}
-      mt={{ base: 2, md: 0 }}
-      px={2}
-      borderRadius={30}
-      alignItems={"center"}
-    >
-      <InputGroup w={"50%"}>
-        <Input
-          variant="unstyled"
-          placeholder="Search..."
-          bg={"secondaryGray.300"}
-          size={"sm"}
-          sx={{
-            width: "auto",
-            height: "40px",
-            padding: "11px 16px 11px 40px",
-            borderRadius: "30px",
-          }}
-        />
-        <InputLeftElement
-          top={"50%"}
-          transform={"translateY(-50%)"}
-          fontSize={20}
-        >
-          <FiSearch />
-        </InputLeftElement>
-      </InputGroup>
-      <Spacer />
-      <ButtonGroup alignItems={"center"} w={"42%"}>
-        <Button variant={"searchicon"}>
-          <FaRegBell />
-          <span className="blind">알림</span>
-        </Button>
-        <Button variant={"searchicon"}>
-          <IoInformationCircleOutline />
-        </Button>
-        <Button variant={"searchicon"}>
-          <FaPalette />
-        </Button>
-        <Button variant={"searchicon"} w={"60px"}>
-          <FaUserCircle
-            width={"40px"}
-            height={"40px"}
-            name="Prosper Otemuyiwa"
-            src="https://bit.ly/prosper-baba"
+    <>
+      <Flex
+        bg={"white"}
+        display={"flex"}
+        h={61}
+        mt={{ sm: 2, md: 0 }}
+        px={2}
+        borderRadius={30}
+        alignItems={"center"}
+      >
+        <InputGroup w={"50%"}>
+          <Input
+            variant="unstyled"
+            placeholder="Search..."
+            bg={"secondaryGray.300"}
+            size={"sm"}
+            sx={{
+              width: "auto",
+              height: "40px",
+              padding: "11px 16px 11px 40px",
+              borderRadius: "30px",
+            }}
           />
-        </Button>
-      </ButtonGroup>
-    </Flex>
+          <InputLeftElement
+            top={"50%"}
+            transform={"translateY(-50%)"}
+            fontSize={20}
+          >
+            <FiSearch />
+          </InputLeftElement>
+        </InputGroup>
+        <Spacer />
+        <ButtonGroup alignItems={"center"} w={"42%"}>
+          <Button variant={"searchicon"}>
+            <FaRegBell />
+            <span className="blind">알림</span>
+          </Button>
+          <Button variant={"searchicon"}>
+            <IoInformationCircleOutline />
+          </Button>
+          <Button variant={"searchicon"}>
+            <FaPalette />
+          </Button>
+          <Button variant={"searchicon"} w={"60px"}>
+            <FaUserCircle
+              width={"40px"}
+              height={"40px"}
+              name="Prosper Otemuyiwa"
+              src="https://bit.ly/prosper-baba"
+            />
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </>
   );
 };
 
